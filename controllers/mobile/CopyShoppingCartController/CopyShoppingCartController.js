e8e0e1bd09137f6c774361d0becfada62d8bf285
Toan Ho <toan.ho@successsoftware.global>
2025-03-08 15:13:41 +0700

    Added checkout 1/4

--- controllers/mobile/CopyShoppingCartController/CopyShoppingCartController.js
+++ controllers/mobile/CopyShoppingCartController/CopyShoppingCartController.js
@@ -1,132 +1,173 @@
 define({  
-    CartProductList: [  
-        { lblDescription : "test", 
-         unitPrice      : 50,
-         DeleteIcon     : 'trashicon.png',
-         MinusIcon      : 'minusicon.png',
-         PlusIcon       : 'plusicon.png',
-         ProductQuantity: '1',
-         img            : 'cartproductimg.png' },
-        { lblDescription : "test",
-         unitPrice	     : 50, 
-         DeleteIcon     : 'trashicon.png',
-         MinusIcon      : 'minusicon.png',
-         PlusIcon       : 'plusicon.png', 
-         ProductQuantity: '1', 
-         img            : 'cartproductimg.png' }, 
-        { lblDescription : "test",
-         unitPrice      : 50,
-         DeleteIcon     : 'trashicon.png',
-         MinusIcon      : 'minusicon.png',
-         PlusIcon       : 'plusicon.png',
-         ProductQuantity: '1',
-         img            : 'cartproductimg.png' }
-    ],
-  
-  CartCarouselProductList:[
-     { CarouselProductImg : "cartproductimg.png", 
-         CarouselProductMade     : "Nature Made",
-          CarouselProductName   : 'Skin With Biotin Softgels',
-         CarouselPoductUnitQuantity      : '90Ct',
-         CarouselProductPrice      : 'Price: $16.79',},
-       { CarouselProductImg : "cartproductimg.png", 
-         CarouselProductMade     : "Nature Made",
-          CarouselProductName   : 'Skin With Biotin Softgels',
-         CarouselPoductUnitQuantity      : '90Ct',
-         CarouselProductPrice      : 'Price: $16.79',},
-       { CarouselProductImg : "cartproductimg.png", 
-         CarouselProductMade     : "Nature Made",
-          CarouselProductName   : 'Skin With Biotin Softgels',
-         CarouselPoductUnitQuantity      : '90Ct',
-         CarouselProductPrice      : 'Price: $16.79',}
+  CartProductList: [  
+    { lblDescription : "test", 
+     unitPrice      : 50,
+     DeleteIcon     : 'trashicon.png',
+     MinusIcon      : 'minusicon.png',
+     PlusIcon       : 'plusicon.png',
+     ProductQuantity: '1',
+     img            : 'cartproductimg.png' },
+    { lblDescription : "test",
+     unitPrice	     : 50, 
+     DeleteIcon     : 'trashicon.png',
+     MinusIcon      : 'minusicon.png',
+     PlusIcon       : 'plusicon.png', 
+     ProductQuantity: '1', 
+     img            : 'cartproductimg.png' }, 
+    { lblDescription : "test",
+     unitPrice      : 50,
+     DeleteIcon     : 'trashicon.png',
+     MinusIcon      : 'minusicon.png',
+     PlusIcon       : 'plusicon.png',
+     ProductQuantity: '1',
+     img            : 'cartproductimg.png' }
+  ],
+  CartCarouselProductList: [
+    { 
+      CarouselProductImg   : "cartproductimg.png", 
+     CarouselProductMade   : "Nature Made",
+     CarouselProductName   : 'Skin With Biotin Softgels',
+     CarouselPoductUnitQuantity      : '90Ct',
+     CarouselProductPrice      : 'Price: $16.79',},
+    { CarouselProductImg : "cartproductimg.png", 
+     CarouselProductMade     : "Nature Made",
+     CarouselProductName   : 'Skin With Biotin Softgels',
+     CarouselPoductUnitQuantity      : '90Ct',
+     CarouselProductPrice      : 'Price: $16.79',},
+    { CarouselProductImg : "cartproductimg.png", 
+     CarouselProductMade     : "Nature Made",
+     CarouselProductName   : 'Skin With Biotin Softgels',
+     CarouselPoductUnitQuantity      : '90Ct',
+     CarouselProductPrice      : 'Price: $16.79',}
   ],
 
-    initProductData: function () {  
-        var scope = this;
+  initProductData: function () {  
+    var scope = this;
 
-        var modifiedData = [];
-        for (var i = 0; i < this.CartProductList.length; i++) {
-            var item = this.CartProductList[i];
+    var modifiedData = [];
+    for (var i = 0; i < this.CartProductList.length; i++) {
+      var item = this.CartProductList[i];
 
-            var newItem = {
-                lblDescription : item.lblDescription,
-                lblCost        : "$" + item.unitPrice.toFixed(2),
-                unitPrice      : item.unitPrice, 
-                DeleteIcon     : item.DeleteIcon,
-                ProductQuantity: item.ProductQuantity,
-                img            : item.img,
-                PlusIcon       : {
-                    src    : item.PlusIcon,
-                    onClick: (function(index) {
-                        return function() {
-                            scope.increaseQuantity(index);
-                        };
-                    })(i)
-                },
-                MinusIcon: {
-                    src    : item.MinusIcon,
-                    onClick: (function(index) {
-                        return function() {
-                            scope.decreaseQuantity(index);
-                        };
-                    })(i)
-                }
+      var newItem = {
+        lblDescription : item.lblDescription,
+        lblCost        : "$" + item.unitPrice.toFixed(2),
+        unitPrice      : item.unitPrice, 
+        DeleteIcon     : item.DeleteIcon,
+        ProductQuantity: item.ProductQuantity,
+        img            : item.img,
+        PlusIcon       : {
+          src    : item.PlusIcon,
+          onClick: (function(index) {
+            return function() {
+              scope.increaseQuantity(index);
             };
-
-            modifiedData.push(newItem);
+          })(i)
+        },
+        MinusIcon: {
+          src    : item.MinusIcon,
+          onClick: (function(index) {
+            return function() {
+              scope.decreaseQuantity(index);
+            };
+          })(i)
         }
+      };
 
-        this.view.ProductSimple.segTemp.setData(modifiedData);
-      	this.view.ProductListCarousel.ProductCarousel.setData(this.CartCarouselProductList);
-      this.calculateTotal();
-    },
-
-    increaseQuantity: function(rowIndex) {
-
-    var segmentData = this.view.ProductSimple.segTemp.data;
-
-
-    var newQuantity = parseInt(segmentData[rowIndex].ProductQuantity, 10) + 1;
-    segmentData[rowIndex].ProductQuantity = newQuantity.toString();
-
-    var unitPrice = segmentData[rowIndex].unitPrice;  
-    segmentData[rowIndex].lblCost = "$" + (unitPrice * newQuantity);
-
-    this.view.ProductSimple.segTemp.setDataAt(segmentData[rowIndex], rowIndex);
-      this.calculateTotal();
-},
-
-decreaseQuantity: function(rowIndex) {
-
-    var segmentData = this.view.ProductSimple.segTemp.data;
-
-
-    var currentQuantity = parseInt(segmentData[rowIndex].ProductQuantity, 10);
-    if (currentQuantity > 1) {
-        var newQuantity = currentQuantity - 1;
-        segmentData[rowIndex].ProductQuantity = newQuantity.toString();
-
-        var unitPrice = segmentData[rowIndex].unitPrice;  
-        segmentData[rowIndex].lblCost = "$" + (unitPrice * newQuantity);
-
-        this.view.ProductSimple.segTemp
-          .setDataAt(segmentData[rowIndex], rowIndex);
-     this.calculateTotal();
+      modifiedData.push(newItem);
     }
-},
- calculateTotal: function() {
-    var segmentData = this.view.ProductSimple.segTemp.data;
+
+    this.view.CartProductList.ProductList.setData(modifiedData);
+    this.view.ProductListCarousel.ProductCarousel.setData(this.CartCarouselProductList);
+    this.calculateTotal();
+  },
+
+  increaseQuantity: function(rowIndex) {
+    var segmentData = this.view.CartProductList.ProductList.data;
+    var newQuantity = parseInt(segmentData[rowIndex].ProductQuantity, 10) + 1;
+
+    segmentData[rowIndex].ProductQuantity = newQuantity.toString();
+    segmentData[rowIndex].lblCost = "$" + (segmentData[rowIndex].unitPrice * newQuantity).toFixed(2);
+
+    // Instead of setData(), use setDataAt() to update only the changed row
+    this.view.CartProductList.ProductList.setDataAt(segmentData[rowIndex], rowIndex);
+    this.calculateTotal();
+
+    // Map the data to the new format
+    var mappedData = segmentData.map(function(item) {
+      return {
+        OrderSummaryItemImg: item.img, 
+        OrderSummaryProductName: item.lblDescription,
+        OrderSummaryProductQty: "Qty " + item.ProductQuantity,  
+        OrderSummaryProductPrice: item.lblCost
+      };
+    });
+
+    // Store the mapped cart data
+    voltmx.store.setItem("UpdatedCartProductList", JSON.stringify(mappedData));
+  },
+
+
+
+  decreaseQuantity: function(rowIndex) {
+    var segmentData = this.view.CartProductList.ProductList.data;
+    var currentQuantity = parseInt(segmentData[rowIndex].ProductQuantity, 10);
+
+    if (currentQuantity > 1) {
+      var newQuantity = currentQuantity - 1;
+      segmentData[rowIndex].ProductQuantity = newQuantity.toString();
+      segmentData[rowIndex].lblCost = "$" + (segmentData[rowIndex].unitPrice * newQuantity).toFixed(2);
+
+      // Use setDataAt() to update only the modified row
+      this.view.CartProductList.ProductList.setDataAt(segmentData[rowIndex], rowIndex);
+      this.calculateTotal();
+
+      // Map the data to the new format
+      var mappedData = segmentData.map(function(item) {
+        return {
+          OrderSummaryItemImg: item.img, 
+          OrderSummaryProductName: item.lblDescription,
+          OrderSummaryProductQty: "Qty " + item.ProductQuantity,  
+          OrderSummaryProductPrice: item.lblCost
+        };
+      });
+
+      // Store the mapped cart data
+      voltmx.store.setItem("UpdatedCartProductList", JSON.stringify(mappedData));
+    }
+  },
+
+
+  calculateTotal: function() {
+    var segmentData = this.view.CartProductList.ProductList.data;
     var totalPrice = 0;
 
     for (var i = 0; i < segmentData.length; i++) {
-        var item = segmentData[i];
-        var quantity = parseInt(item.ProductQuantity, 10);
-        var unitPrice = item.unitPrice;
-        totalPrice += quantity * unitPrice;
+      var item = segmentData[i];
+      var quantity = parseInt(item.ProductQuantity, 10);
+      var unitPrice = item.unitPrice;
+      totalPrice += quantity * unitPrice;
     }
-  
-        this.view.CheckoutAndPromoteContainer.
-        EstContainer.EstAmount.text = "$" + totalPrice.toFixed(2);
-}
+    this.view.CheckoutAndPromoteContainer.
+    EstContainer.EstAmount.text = "$" + totalPrice.toFixed(2);
+  },
+  navigateToCheckout:function () {
+    var taxText = this.view.CheckoutAndPromoteContainer.TaxContainer.TaxAmountLabel.text;
+    var cartTotalText = this.view.CheckoutAndPromoteContainer.EstContainer.EstAmount.text;
+
+    // Remove "$" and "%" symbols, then convert to numbers
+    var taxPercentage = parseFloat(taxText.replace("%", "").trim());
+    var cartTotalPrice = parseFloat(cartTotalText.replace("$", "").trim());
+
+    // Convert tax percentage to decimal and calculate the final total
+    var taxAmount = (cartTotalPrice * taxPercentage) / 100;
+    var finalTotalPrice = cartTotalPrice + taxAmount;
+
+    // Store the updated total price
+    voltmx.store.setItem("CartTotalPrice", "$" + finalTotalPrice.toFixed(2));
+
+    voltmx.store.setItem("CartItemQuantity",this.CartProductList.length);
+    var navObj = new voltmx.mvc.Navigation("CheckoutAddress");
+    navObj.navigate();
+  }
 
 });
