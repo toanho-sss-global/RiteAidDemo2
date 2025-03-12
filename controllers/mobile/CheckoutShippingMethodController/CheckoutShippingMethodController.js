10605a015b2d6010f95612cbd870bdffcea140a3
Anh Le <anh.le@successsoftware.local>
2025-03-12 14:13:22 +0700

    integrate api SetShippingMethod SetShippingAddress SetBillingAddress getPickupStores

--- controllers/mobile/CheckoutShippingMethodController/CheckoutShippingMethodController.js
+++ controllers/mobile/CheckoutShippingMethodController/CheckoutShippingMethodController.js
@@ -40,14 +40,18 @@ define({
   },
   
   ApiShipingMethod: function() {
+    var ShippingOptions = this.view.CheckoutShippingMethodContainer.ShippingModeContainer.ShippingOptions.selectedKey;
     var httpclient = new voltmx.net.HttpRequest();
+    var token =  localStorage.getItem("vendure-auth-token");
+    
     httpclient.open(constants.HTTP_METHOD_POST,"https://vendure.demo.universalcommerce.io/shop-api");
     httpclient.setRequestHeader("Content-Type", "application/json");
+    httpclient.setRequestHeader("Authorization",`Bearer ${token}`);
     
     var jsonStr2 = JSON.stringify({
     "query": "mutation SetOrderShippingMethod($shippingMethodIds: [ID!]!) { setOrderShippingMethod(shippingMethodId: $shippingMethodIds) { ... on Order { id createdAt updatedAt type orderPlacedAt code state active totalQuantity subTotal subTotalWithTax currencyCode shipping shippingWithTax total totalWithTax } ... on OrderModificationError { errorCode message } ... on IneligibleShippingMethodError { errorCode message } ... on NoActiveOrderError { errorCode message } } }",
     "variables": {
-        "shippingMethodIds": "4"
+        "shippingMethodIds": ShippingOptions
     }
     });
     httpclient.send(jsonStr2);
@@ -62,8 +66,48 @@ define({
           }
         }
   	}
+  },
+  
+  ApigetPickupStores: function() {
+    var httpclient = new voltmx.net.HttpRequest();
+    var token =  localStorage.getItem("vendure-auth-token");
+  
+  var arrayVariant = ["92", "90", "89"]
+    
+    httpclient.open(constants.HTTP_METHOD_POST,"https://vendure.demo.universalcommerce.io/shop-api");
+    httpclient.setRequestHeader("Content-Type", "application/json");
+    httpclient.setRequestHeader("Authorization",`Bearer ${token}`);
+  
+    var jsonStr2 = JSON.stringify({
+  "query": "query GetProductVariants($ids: [String!]!) { productVariants(options: { filter: { id: { in: $ids } } }) { items { id name sku price featuredAsset { preview } channels { id token code seller { id name customFields { googleMapLink latitude longitude } } } } } }",
+  "variables": {
+    "ids": arrayVariant
   }
+}
+);
+    httpclient.send(jsonStr2);
+  
+  
+  httpclient.onReadyStateChange = function () {
+        if (httpclient.readyState === 4 && httpclient.status === 200) {
+          var response = JSON.parse(httpclient.response);
+          if(response) {
+            console.log("Api Map")
+          }
+        }
+  	}
+},
+  
+  checkMehtod: function() {
+      var ShippingOptions = this.view.CheckoutShippingMethodContainer.ShippingModeContainer.ShippingOptions.selectedKey;
+  if (ShippingOptions == 4) {
+    this.ApiShipingMethod();
+  } else {
+    this.ApigetPickupStores();
+  }
+}
 
 
+ 
 
 });
