550939e1929cc072d7eccfaf6b87939aac656fff
Khoi Ho <khoi.ho@successsoftware.global>
2025-03-14 12:03:32 +0700

    add QR code API

--- controllers/mobile/Form3Controller/Form3Controller.js
+++ controllers/mobile/Form3Controller/Form3Controller.js
@@ -1,37 +1,14 @@
 define({ 
-  qrData: {
-    "orderId"  : "203",
-    "orderDate": "2025-03-13T07:53:13.082Z",
-    "status"   : "PaymentSettled",
-    "total"    : "USD 1919.40",
-    "items"    : [
-      {
-        "name"    : "Qunol Turmeric & Ginger Gummy - 60 ct",
-        "quantity": 40
-      },
-      {
-        "name"    : "Listerine Total Care Anticavity Fluoride Mouthwash - Fresh Mint, 1 lt",
-        "quantity": 34
-      },
-      {
-        "name"    : "Nature Made Vitamin D3 Softgels, 50mcg - 90 ct",
-        "quantity": 46
-      },
-      {
-        "name"    : "Mott's 100% Original Apple Juice Boxes, 6.75 fl oz - 8 pk",
-        "quantity": 3
-      }
-    ],
-    "shipping": {
-      "name"   : "AA BB",
-      "address": "13 adk, HCM"
+  qrData: {},
+  currentProductID : "",
+  getCurrentProductID: function() {
+    if (this.navigationContext) {
+      this.currentProductID = this.navigationContext.variable_product_Id;
+      console.log("Received variable_product_Id:", this.variableProductId);
     }
   },
   getOrderDetails: function () {
-    var data = JSON.stringify(this.qrData);
-    console.log("DATA STRING: ", data);
-	this.view.qrcodegenerator.dataToEncode = data.replace(/'/g, "\\'");
-    this.view.qrcodegenerator.generate();
+
     var httpclient = new voltmx.net.HttpRequest();
     httpclient.open(constants.HTTP_METHOD_POST, 
                     "https://vendure.demo.universalcommerce.io/shop-api");
@@ -41,10 +18,11 @@ define({
     var jsonStr = JSON.stringify({
       "query": "query GetOrder($orderId: ID!) { order(id: $orderId) { id orderPlacedAt subTotalWithTax shippingWithTax totalWithTax currencyCode state payments { metadata } customFields { deliveryType } lines { id linePriceWithTax quantity productVariant { id name priceWithTax featuredAsset { preview } currencyCode } } shippingAddress { fullName phoneNumber streetLine1 streetLine2 city province postalCode countryCode country } billingAddress { fullName phoneNumber streetLine1 streetLine2 city province postalCode countryCode country } } }",
       "variables": {
-        "orderId": "112"
+        "orderId": this.currentProductID
       }
     });
     httpclient.send(jsonStr);
+    
 
     httpclient.onReadyStateChange = function () {
       if (httpclient.readyState === 4) {
@@ -53,29 +31,51 @@ define({
           var orderDetail = response.data.order;
 
           console.log("Order Detail: ", orderDetail);
+          this.qrData.orderDate = orderDetail.orderPlacedAt;
           var formattedDate =  this.formatDate(orderDetail.orderPlacedAt);
 
           // Update UI after the response is received
           this.view.OrderStatusLabel.text = orderDetail.state;
+          this.qrData.status = orderDetail.state;
+          
+            var skin = "";
+        if (orderDetail.state === "Cancelled") {
+            skin = "sknCancelled";
+        } else if (orderDetail.state === "Delivered") {
+            skin = "sknDelivered";
+        } else {
+            skin = "sknPaymentSettled";
+        }
+    this.view.OrderStatusLabel.skin = skin;
+          
           this.view.OrderIDText.text = orderDetail.id;
+          this.qrData.orderId = orderDetail.id;
           this.view.OrderDateText.text = formattedDate;
           this.view.OrderSubtotalText.text = "$" + orderDetail.subTotalWithTax;
           this.view.OrderShippingText.text = "$" + orderDetail.shippingWithTax;
           this.view.OrderTotalPrice.text = "$" + orderDetail.totalWithTax;
-          this.view.DeliveryTypeText.text = orderDetail.state;
+          this.qrData.total ="USD" + orderDetail.totalWithTax;
+          this.view.DeliveryTypeText.text = orderDetail.customFields.deliveryType ? orderDetail.customFields.deliveryType : "Not Specified";
+
           var mappedData = orderDetail.lines.map(item => ({
             ItemOrderedImg: item.productVariant.featuredAsset.preview, 
-            ItemOrderedName: this.truncateText(item.productVariant.name, 8), 
+            ItemOrderedName: this.truncateText(item.productVariant.name, 7), 
             ItemOrderedQuantity: item.quantity, 
             ItemOrderedUnitPrice: "$" + (item.productVariant.priceWithTax / 100).toFixed(2),
             ItemOrderedTotalPrice: "$" + (item.linePriceWithTax / 100).toFixed(2)
           }));
+          var mappedQRCodeItemData = orderDetail.lines.map(productItem => ({
+            name: this.truncateText(productItem.productVariant.name, 8),
+            quantity: productItem.quantity
+          }));
+          this.qrData.items = mappedQRCodeItemData;
+
           this.view.OrderDetailContainer.ItemOrderedContainer.ItemsOrderedListCtn.ItemOrderedList.setData(mappedData);
 
           this.view.OrderFullNameLabel.text = orderDetail.shippingAddress.fullName;
           this.view.OrderPhoneLabel.text = orderDetail.shippingAddress.phoneNumber;
-          this.view.OrderAddress1Label.text = orderDetail.shippingAddress.streetLine1;
-          this.view.OrderAddress2Label.text = orderDetail.shippingAddress.streetLine2;
+          this.view.OrderAddress1Label.text = this.truncateText(orderDetail.shippingAddress.streetLine1, 10), 
+          this.view.OrderAddress2Label.text = this.truncateText(orderDetail.shippingAddress.streetLine2, 10), 
           this.view.OrderCityStateZipCodeLabel.text =
             orderDetail.shippingAddress.city + ", " + orderDetail.shippingAddress.province + " " + orderDetail.shippingAddress.postalCode;
           this.view.OrderCountryLabel.text = orderDetail.shippingAddress.country;
@@ -87,11 +87,15 @@ define({
           this.view.OrderCityStateZipCode2.text =
             orderDetail.billingAddress.city + ", " + orderDetail.billingAddress.province + " " + orderDetail.billingAddress.postalCode;
           this.view.OrderCountryLabel2.text = orderDetail.billingAddress.country;
+
+          var data = JSON.stringify(this.qrData);
+          this.view.qrcodegenerator.dataToEncode = data.replace(/'/g, "\\'");
+          this.view.qrcodegenerator.generate();
         } else {
           console.error("API Error:", httpclient.status, httpclient.responseText);
         }
       }
-    }.bind(this); // Ensure `this` refers to the current object
+    }.bind(this); 
 
   },
 
