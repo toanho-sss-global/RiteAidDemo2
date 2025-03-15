00351f8778605186207854d9e06735a12a56c407
Nhan Pham <nhan.pham@successsoftware.local>
2025-03-15 10:21:38 +0700

    update

--- controllers/mobile/LoginController/LoginController.js
+++ controllers/mobile/LoginController/LoginController.js
@@ -47,12 +47,12 @@ define({
   },
   
   LoginApi: function () {
-    var email = this.view.txtBoxEmail.text.trim();
-    var password = this.view.txtBoxPassword.text.trim();
+//     var email = this.view.txtBoxEmail.text.trim();
+//     var password = this.view.txtBoxPassword.text.trim();
     var self = this;
 
-    //  var email = "danny@cnetric.com";
-    // var password = "12345678";
+     var email = "danny@cnetric.com";
+    var password = "12345678";
 
     if (this.validateLogin(email, password)) {
       var httpclient = new voltmx.net.HttpRequest();
