212a14290353aaace6dbb1b93af6010d4664b784
Anh Le <anh.le@successsoftware.local>
2025-03-15 23:43:14 +0700

    integrate SearchProduct Api

--- controllers/mobile/ProductListController/ProductListController.js
+++ controllers/mobile/ProductListController/ProductListController.js
@@ -1,29 +1,29 @@
 define({
   ProductList: [],
-  
+
   getCartBadgeCount: function () {
     var url = "https://vendure.demo.universalcommerce.io/shop-api";
     var token = localStorage.getItem("vendure-auth-token");
-    
+
     var httpclient = new voltmx.net.HttpRequest();
     httpclient.open(constants.HTTP_METHOD_POST, url);
     httpclient.setRequestHeader("Content-Type", "application/json");
-	httpclient.setRequestHeader("Authorization", "Bearer " + token);
-    
-      var jsonStr2 = JSON.stringify({
-    	"query": "query ActiveOrder { activeOrder { id lines { productVariant { id } } } }"
-		});
-      
+    httpclient.setRequestHeader("Authorization", "Bearer " + token);
+
+    var jsonStr2 = JSON.stringify({
+      "query": "query ActiveOrder { activeOrder { id lines { productVariant { id } } } }"
+    });
+
     httpclient.send(jsonStr2);
     httpclient.onReadyStateChange = function () {
-        if (httpclient.readyState === 4 && httpclient.status === 200) {
-          var response = JSON.parse(httpclient.response);
-		  var countItem = 0;
-          if (response.data && response.data.activeOrder && response.data.activeOrder.lines) {
-        	countItem = response.data.activeOrder.lines.length;
-          }
-          localStorage.setItem("count-item-in-cart", countItem);
+      if (httpclient.readyState === 4 && httpclient.status === 200) {
+        var response = JSON.parse(httpclient.response);
+        var countItem = 0;
+        if (response.data && response.data.activeOrder && response.data.activeOrder.lines) {
+          countItem = response.data.activeOrder.lines.length;
         }
+        localStorage.setItem("count-item-in-cart", countItem);
+      }
     }
   },
 
@@ -77,28 +77,94 @@ define({
   },
 
   handleProducts: function (data) {
-    if (!data.products.items || !Array.isArray(data.products.items)) {
+    if (!data.hits || !Array.isArray(data.hits)) {
       return;
     }
 
-    var formattedProducts = data.products.items.map(item => {
+    var formattedProducts = data.hits.map(item => {
       var firstVariant = item.variants && item.variants.length > 0 ? item.variants[0] : {};
       var firstFacet = item.facetValues && item.facetValues.length > 0 ? item.facetValues[0].name : "Unknown";
+      var DiscountItem = item.Promotions && item.Promotions.length > 0 ? item.Promotions : [];
+
+      var price = firstVariant.priceWithTax ? (firstVariant.priceWithTax / 100).toFixed(2) : "0";
+      var Cost = parseFloat(price);
+      var number = parseInt(DiscountItem[0]) || 100;
+      var CostDiscount = Cost * (number / 100);
+
 
       return {
         id: item.id,
         lblDescription: firstVariant.name || "No Name",
-        lblCost: firstVariant.priceWithTax ? `$${(firstVariant.priceWithTax / 100).toFixed(2)}` : "N/A",
+        lblCost: `$${Cost.toFixed(2)}`,
         lblCategory: firstFacet,
-        lblDiscount: "",
-        lblCostDiscount: "",
-        img: item.featuredAsset.preview ? item.featuredAsset.preview : "default.png"
+        lblDiscount: DiscountItem[0],
+        lblCostDiscount: (number > 0 && number < 100) ? `$${CostDiscount.toFixed(2)}` : "",
+        img: item.featuredAsset.preview ? item.featuredAsset.preview : "default.png",
+        lblSpanCost: (number > 0 && number < 100) ? "------------------" : "",
+
       };
     });
 
     this.ProductList = formattedProducts;
-    console.log("Update product list:", this.ProductList);
-
     this.initProductData();
-  }
+  },
+
+  SearchProduct: function(keywork) {
+    var self = kony.mvc.getController("ProductList", true);     
+    var httpclient = new voltmx.net.HttpRequest();
+    var url = `https://3BP6P78G2Y-dsn.algolia.net/1/indexes/Riteaids`;
+
+    if (keywork) {
+      url +=  `?filters=Promotions:"${keywork}% off"`;
+      //           url += `?query=${encodeURIComponent(keywork)}`  `?filters=Promotions:"90${keywork}% off"`;
+    }  
+    var token = "d3BzYWRtaW46d3BzYWRtaW4=";
+
+    httpclient.open(constants.HTTP_METHOD_GET, url);
+    httpclient.setRequestHeader("X-Algolia-API-Key", `1903a10f4bc35dca44f99e43d8c51a99`);
+    httpclient.setRequestHeader("X-Algolia-Application-Id", `3BP6P78G2Y`);
+
+
+    httpclient.onReadyStateChange = function () {
+      if (httpclient.readyState === 4) {
+        if (httpclient.status === 200) {
+          try {
+            var response = JSON.parse(httpclient.response);
+            if (response) {
+              self.handleProducts(response);
+            }
+          } catch (error) {
+            return error;
+          }
+        } 
+      }
+    }
+    httpclient.send();
+  },
+
+
+  SelectionBox: function() { 
+    setTimeout(function () {    
+      var self = kony.mvc.getController("ProductList", true);     
+
+      let selectkey = localStorage.getItem("selectedkey");
+
+      if (selectkey) {
+        self.view.Content.FlexFilterGroup.ListFilter.selectedKey = selectkey;
+        localStorage.removeItem("selectedkey");
+      } else {
+        selectkey = self.view.Content.FlexFilterGroup.ListFilter.selectedKey;
+      }
+
+      if(selectkey === "1") {
+        self.SearchProduct();
+      } else if(selectkey === "2") {
+        self.SearchProduct(50);
+      }
+    }, 500);
+
+  },
+
+
+
 });
